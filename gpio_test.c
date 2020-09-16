cf97d6ccbe23fed0d660b461cf714c3e58c6ee46
Tyler Warren <tylerdwarren98@gmail.com>
2020-09-16 19:39:51 +0100

    Added GPIO reading to gpio_test

--- gpio_test.c
+++ gpio_test.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// Compile with gcc -Wall -pthread gpio_test.c -lpigpio -lrt
+
 int main() {
     if (gpioInitialise() < 0)
     {
@@ -14,10 +16,17 @@ int main() {
     }
 
     gpioSetMode(17, PI_OUTPUT);
+    gpioSetMode(27, PI_INPUT);
     gpioSetPullUpDown(17, PI_PUD_DOWN);
+    gpioSetPullUpDown(27, PI_PUD_UP); 
     gpioWrite(17, 1);
     sleep(2);
     gpioWrite(17, 0);
+
+    while (1) {
+        printf("GPIO27 is level %d\n", gpioRead(27));
+        sleep(1);
+    }
 
     return 0;
 
