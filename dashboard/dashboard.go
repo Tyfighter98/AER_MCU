c5437c88edc9034e186c33a29f59fb2e2145d34d
Tyler Warren <tylerdwarren98@gmail.com>
2020-11-18 18:02:00 -0800

    Added table of values

--- dashboard/dashboard.go
+++ dashboard/dashboard.go
@@ -7,7 +7,6 @@ import (
 	"strconv"
 	"strings"
 	"text/template"
-	"time"
 
 	"github.com/go-echarts/go-echarts/charts"
 	"github.com/go-echarts/go-echarts/opts"
@@ -23,9 +22,11 @@ type Welcome struct {
 }
 
 type LogData struct {
-	time    []string
-	trigger []opts.LineData
-	bspd    []opts.LineData
+	Time        []string
+	TriggerPlot []opts.LineData
+	Trigger     []int
+	BspdPlot    []opts.LineData
+	Bspd        []int
 }
 
 // generate random data for line chart
@@ -43,43 +44,43 @@ func httpserver(w http.ResponseWriter, _ *http.Request) {
 	line := charts.NewLine()
 	// set some global options like Title/Legend/ToolTip or anything else
 	line.SetGlobalOptions(
-		charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWesteros, Width: "900px", Height: "250px", ChartID: "trigger"}),
+		charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWesteros, Width: "900px", Height: "250px", ChartID: "Trigger"}),
 		charts.WithTitleOpts(opts.Title{
 			Title:    "Line example in Westeros theme",
-			Subtitle: "Line chart rendered by the http server this time",
+			Subtitle: "Line chart rendered by the http server this Time",
 		}))
 
 	// Put data into instance
-	line.SetXAxis(logData.time).
-		AddSeries("Category A", logData.trigger).
+	line.SetXAxis(logData.Time).
+		AddSeries("Category A", logData.TriggerPlot).
 		SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: true}))
 	line.Render(w)
 
 	line2 := charts.NewLine()
 	// set some global options like Title/Legend/ToolTip or anything else
 	line2.SetGlobalOptions(
-		charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWesteros, Width: "900px", Height: "250px", ChartID: "bspd"}),
+		charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWesteros, Width: "900px", Height: "250px", ChartID: "Bspd"}),
 		charts.WithTitleOpts(opts.Title{
 			Title:    "Line example in Westeros theme",
-			Subtitle: "Line chart rendered by the http server this time",
+			Subtitle: "Line chart rendered by the http server this Time",
 		}))
 
 	// Put data into instance
-	line2.SetXAxis(logData.time).
-		AddSeries("Category B", logData.bspd).
+	line2.SetXAxis(logData.Time).
+		AddSeries("Category B", logData.BspdPlot).
 		SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: true}))
 	line2.Render(w)
 
 	templates := template.Must(template.ParseFiles("templates/index.html"))
-	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
+	// welcome := Welcome{"Anonymous", Time.Now().Format(Time.Stamp)}
 
-	if err := templates.ExecuteTemplate(w, "index.html", welcome); err != nil {
+	if err := templates.ExecuteTemplate(w, "index.html", logData); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func readLog() LogData {
-	logData := LogData{make([]string, numLines), make([]opts.LineData, numLines), make([]opts.LineData, numLines)}
+	logData := LogData{make([]string, numLines), make([]opts.LineData, numLines), make([]int, numLines), make([]opts.LineData, numLines), make([]int, numLines)}
 	file, err := os.Open("test.csv")
 	if err != nil {
 		panic(err)
@@ -100,17 +101,19 @@ func readLog() LogData {
 	for i := 0; i < numLines; i++ {
 		// fmt.Printf("%d: %s\n", i, lines[(len(lines)-2)-i])
 		values := strings.Split(lines[(len(lines)-2)-i], ",")
-		logData.time[(numLines-1)-i] = values[0]
-		trigger, err := strconv.Atoi(values[1])
+		logData.Time[(numLines-1)-i] = values[0]
+		Trigger, err := strconv.Atoi(values[1])
 		if err != nil {
 			panic(err)
 		}
-		bspd, err := strconv.Atoi(values[2])
+		Bspd, err := strconv.Atoi(values[2])
 		if err != nil {
 			panic(err)
 		}
-		logData.trigger[(numLines-1)-i] = opts.LineData{Value: trigger}
-		logData.bspd[(numLines-1)-i] = opts.LineData{Value: bspd}
+		logData.TriggerPlot[(numLines-1)-i] = opts.LineData{Value: Trigger}
+		logData.Trigger[(numLines-1)-i] = Trigger
+		logData.BspdPlot[(numLines-1)-i] = opts.LineData{Value: Bspd}
+		logData.Bspd[(numLines-1)-i] = Bspd
 	}
 
 	return logData
